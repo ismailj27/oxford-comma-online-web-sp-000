3d99394ad0f7bede89c809c904831d62c9b6d640
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-28 13:08:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
-  array[-1] = "and " + array[-1]
-  array = array.join(", ")
-  return array
+  if array.length == 1
+    return array[0]
+  else
+    array[-1] = "and " + array[-1]
+    array = array.join(", ")
+    return array
+  end
 end
